015cc26145b96185d0df48ad03d747caff82368b
paulogenova <32643036+paulogenova@users.noreply.github.com>
2017-10-23 23:39:49 +0800

    update

--- weather.js
+++ weather.js
@@ -12,30 +12,39 @@ $(document).ready(function () {
             var temp = weather.main.temp;
             var description = weather.weather[0].description;
             var humidity = weather.main.humidity;
-            $("#temperature").html(temp + " Kelvin");
+            var tempC = (temp - 273.15).toFixed(2);
+            $("#temperature").html(tempC + " Celsius");
             $("#description").html(description);
             $("#humidity").html(humidity + "%");
 
-            //conversion of units from K to C/F
+            //conversion of units from C<->F upon click
 
-            var degC = (parseFloat($("#temperature").text()) - 273.15).toFixed(2);
+       /*     var degC = (parseFloat($("#temperature").text()) - 273.15).toFixed(2);*/
 
             $("#degC").click(function () {
-                return $("#temperature").html(degC + " &deg;C");
+                return $("#temperature").html(tempC + " &deg;C");
             });
 
             $("#degF").click(function () {
-                var degF = ((degC * 1.8) + 32).toFixed(2);
+                var degF = ((tempC * 1.8) + 32).toFixed(2);
                 $("#temperature").html(degF + " &deg;F");
             });
 
         };
+        
+        //lead-in statement upon city name input
 
         var leadingtext = 'Current weather in ' + '<span>' + cityName + '</span>' + ' is as follows:'
 
         $('.leading-text').html(leadingtext);;
 
         $('h3, h4').addClass('animated fadeInUp');
-
+        
+    });
+    
+    $("input").keydown(function(event){
+        if(event.keyCode == 13){
+            $("button").click();
+        }
     });
 });
